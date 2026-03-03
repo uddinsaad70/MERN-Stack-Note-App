b2a8d5ebf648b912179c74775bbed34e48f62351
baagdaas <saad.cse.kuet@gmail.com>
2026-03-03 23:07:37 +0600

    feat: backend setup with es modules, basic routes, and README

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/api/notes', (req, res) => {
+    res.status(200).send("you got 15 notes");
+});
+
+app.post('/api/notes', (req, res) => {
+    res.status(201).json({ message: "Note created successfully"})
+});
+
+app.put('/api/notes/:id', (req, res) => {
+    res.status(200).json({ message: "Note updated successfully" })
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    res.status(200).json({ message: "Note deleted successfully" })
+});
+
+app.listen(5001, () => {
+    console.log('Server is running on port 5001');
+});
