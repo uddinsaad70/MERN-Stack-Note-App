f682d53c8b8a96765c29c25d8af010fed4519f8d
baagdaas <saad.cse.kuet@gmail.com>
2026-03-04 10:11:02 +0600

    refactor: implement MVC folder structure with routes and controllers

---
+++ backend/src/routes/notesroutes.js
@@ -0,0 +1,13 @@
+import e from "express";
+import express from "express";
+import { getAllNotes, createNote, updateNote, deleteNote} from "../controllers/notesController.js";
+
+const router = express.Router();
+
+router.get('/', getAllNotes);
+router.post('/', createNote);
+router.put('/:id', updateNote);
+router.delete('/:id', deleteNote);
+
+
+export default router;
