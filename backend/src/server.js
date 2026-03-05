3a82458b0aa5ae31e422fd9b3d3cad3c2568df2e
baagdaas <saad.cse.kuet@gmail.com>
2026-03-05 14:04:57 +0600

    feat: setup MongoDB connection and create Note model

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import notesRoutes from './routes/notesroutes.js';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5001;
+connectDB();
 
 app.use('/api/notes', notesRoutes);
 
-app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
+
+// mongodb+srv://saadcsekuet_db_user:TliAjHGT7ut93Zuc@cluster0.bxctic5.mongodb.net/?appName=Cluster0
