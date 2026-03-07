da4009d3bc50f5fa885de788c40c547cae5b9603
baagdaas <saad.cse.kuet@gmail.com>
2026-03-07 15:15:32 +0600

    feat: add rate limiting with Upstash Redis

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,12 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import dotenv from 'dotenv';
+dotenv.config();
+
+//create a ratelimiter that allows 5 requests per 10 seconds
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(100, "60 s"),
+});
+
+export default ratelimit;   
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,17 +2,47 @@ import express from 'express';
 import notesRoutes from './routes/notesroutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
-connectDB();
+
+
 //middleware
 app.use(express.json());
+
+app.use(rateLimiter);
+
+// app.use((req, res, next) => {
+//     let methodColor;
+//     switch (req.method) {
+//         case 'GET':
+//             methodColor = colors.green;
+//             break;
+//         case 'POST':
+//             methodColor = colors.blue;
+//             break;
+//         case 'PUT':
+//             methodColor = colors.yellow;
+//             break;
+//         case 'DELETE':
+//             methodColor = colors.red;
+//             break;
+//         default:            
+//             methodColor = colors.white;
+//     }
+//     console.log(methodColor(`${req.method} ${req.url}`));
+//     next();
+// });
+
 app.use('/api/notes', notesRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 });
 
 
