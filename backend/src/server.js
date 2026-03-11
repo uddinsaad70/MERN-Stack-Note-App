1598146f4a422da93d3c70dbce3678548a9cde42
baagdaas <saad.cse.kuet@gmail.com>
2026-03-11 15:37:47 +0600

    chore: prepare for production deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import colors from 'colors';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs'; 
 import notesRoutes from './routes/notesroutes.js';
 import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
@@ -11,12 +13,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.resolve(__filename);
 
 //middleware
-const corsOptions = {
-    origin: ['http://localhost:5173','http://localhost:5174'],
+if(process.env.NODE_ENV !== "production"){
+    const corsOptions = {
+        origin: ['http://localhost:5173','http://localhost:5174'],
+    }
+    app.use(cors(corsOptions));
 }
-app.use(cors(corsOptions));
+
+
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -44,11 +52,29 @@ app.use(rateLimiter);
 
 app.use('/api/notes', notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    const frontendPath = path.join(__dirname, "../../frontend/dist");
+    const indexPath = path.join(__dirname, "../../frontend/dist/index.html");
+    
+    console.log("📁 Frontend path:", frontendPath);
+    console.log("📄 Index path:", indexPath);
+    console.log("✅ Index exists:", fs.existsSync(indexPath)); 
+    
+    app.use(express.static(frontendPath));
+
+    app.get("*", (req, res) => {
+        res.sendFile(indexPath);
+    })
+}
+
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+        console.log(`Server is running on port ${PORT}`.green.bold);
     });
+}).catch(err => {
+    console.error("Database connection failed:".red.bold, err);
+    process.exit(1);
 });
 
 
-// mongodb+srv://saadcsekuet_db_user:TliAjHGT7ut93Zuc@cluster0.bxctic5.mongodb.net/?appName=Cluster0
+// // mongodb+srv://saadcsekuet_db_user:TliAjHGT7ut93Zuc@cluster0.bxctic5.mongodb.net/?appName=Cluster0
